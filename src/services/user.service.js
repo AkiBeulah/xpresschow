afe62aed6e094783da19f24e6a5e1679b1777d81
Akindele Beulah <akibeulah@gmail.com>
2020-06-18 02:26:08 +0100

    updates API_URL

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import authHeader from "./auth-header"
 
-const API_URL = 'http://localhost:3001/';
+const API_URL = 'https://xpresschow-api.herokuapp.com/';
 
 class UserService {
   getPublicContent() {
