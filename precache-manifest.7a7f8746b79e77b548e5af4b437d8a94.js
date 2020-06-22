e7dae468880cc10d6acbc7d17547db2fd723d116
Akindele Beulah <akibeulah@gmail.com>
2020-06-22 03:26:18 +0100

    Updates

---
+++ precache-manifest.7a7f8746b79e77b548e5af4b437d8a94.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7451c346e63be2aab35f9ca3c36bf510",
+    "url": "/xpresschow/index.html"
+  },
+  {
+    "revision": "040485dc6c1913cc14de",
+    "url": "/xpresschow/static/css/2.fdf9ceb3.chunk.css"
+  },
+  {
+    "revision": "77dd290efbc639720289",
+    "url": "/xpresschow/static/css/main.1830c5a6.chunk.css"
+  },
+  {
+    "revision": "040485dc6c1913cc14de",
+    "url": "/xpresschow/static/js/2.f57eb529.chunk.js"
+  },
+  {
+    "revision": "82f677d910c951dde33bc2ac7bb34b46",
+    "url": "/xpresschow/static/js/2.f57eb529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77dd290efbc639720289",
+    "url": "/xpresschow/static/js/main.0f1c9125.chunk.js"
+  },
+  {
+    "revision": "af09804945ab4685cdfb",
+    "url": "/xpresschow/static/js/runtime-main.8be5361d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/xpresschow/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/xpresschow/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/xpresschow/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/xpresschow/static/media/slick.f97e3bbf.svg"
+  }
+]);
