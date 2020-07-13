aa9a870e6affc5aa7a01d8f0da7d06f922799e86
Akindele Beulah <akibeulah@gmail.com>
2020-07-13 03:53:02 +0100

    Updated scripts and API_URL for deployment

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 import authHeader from "./auth-header"
 
-const API_URL = `http://localhost:3001/api/v1/`
-// const API_URL = 'https://xpresschow-api.herokuapp.com/api/v1/';
+// const API_URL = `http://localhost:3001/api/v1/`
+const API_URL = 'https://xpresschow-api.herokuapp.com/api/v1/';
 
 
 class UserService {
--- src/services/vendor.service.js
+++ src/services/vendor.service.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 import authHeader from "./auth-header"
 
-const API_URL = `http://localhost:3001/api/v1/`
-const IMG_URL = "https://api.imgbb.com/1/upload"
-// const API_URL = 'https://xpresschow-api.herokuapp.com/api/v1/';
+// const API_URL = `http://localhost:3001/api/v1/`
+const API_URL = 'https://xpresschow-api.herokuapp.com/api/v1/';
 
 
 class VendorService {
